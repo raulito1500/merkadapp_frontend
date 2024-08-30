b206bc845716c0aba82562e9b266392b80639755
Raúl Cuéllar <raulito1500@gmail.com>
2024-08-29 21:17:13 -0500

    Header refactor

--- src/App/AppHeader/index.js
+++ src/App/AppHeader/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { AppContext } from "../Context";
 import { NavLink } from "react-router-dom"
-import { Container, Dropdown, Spinner } from "react-bootstrap"
+import { Container, Dropdown, Nav, Navbar, Spinner } from "react-bootstrap"
 import { useAuth } from "../Context/auth";
 
 const routes = [
@@ -18,35 +18,17 @@ function AppHeader() {
     return (
         <div className="shadowHeader">
             <header className="pt-4 pt-sm-1 pb-4 mb-3 bg-primary">
-                <Container>
-                    <div className="d-flex flex-column align-items-center flex-sm-row justify-content-sm-between" >
-                        <a href="/" className="d-flex align-items-start text-decoration-none text-white main-logo">
+                <Navbar expand="md">
+                    <Container>
+                        <Navbar.Toggle aria-controls="main-nav-bar" className="text-white border-0 order-md-1" >
+                            <i class="bi bi-list"></i>
+                        </Navbar.Toggle>
+                        <Navbar.Brand href="/" className="text-decoration-none text-white main-logo order-md-2">
                             <img src="logo.svg" className="me-2" alt="Merkadapp logo" />
-                            <span className="fs-2">Merkadapp</span>
-                        </a>
-                        <ul className="nav mt-3 mb-3 p-2 mb-0 bg-white-25 rounded-3">
-                            {routes.map((route, index) => {
-                                if (route.private && !auth.isAuthenticated()) return null;
-                                else {
-                                    return (
-                                        <li
-                                            key={index}>
-                                            <NavLink
-                                                to={route.to}
-                                                className="nav-link px-2 link-light">
-                                                {route.text}</NavLink></li>
-                                    )
-                                }
-                            }
-                            )}
-                        </ul>
-                        {loading ? <Spinner
-                            animation="border"
-                            variant="primary" 
-                            role="status" className="position-fixed bottom-0 end-0 z-3">
-                            <span className="visually-hidden">Loading...</span>
-                        </Spinner> : ""}
-                        {auth.user ? <Dropdown>
+                            <span className="short"></span>
+                            <span className="full fs-2">Merkadapp</span>
+                        </Navbar.Brand>
+                        {auth.user ? <Dropdown className="order-md-4">
                             <Dropdown.Toggle
                                 variant="none"
                                 className="d-block link-light text-decoration-none dropdown-toggle"
@@ -59,10 +41,34 @@ function AppHeader() {
                         </Dropdown> :
                             <NavLink
                                 to="/login"
-                                className="btn btn-outline-light">Log in</NavLink>
+                                className="btn btn-outline-light order-md-4">Log in</NavLink>
                         }
-                    </div>
-                </Container>
+                        <Navbar.Collapse id="main-nav-bar" className="order-md-3">
+                            <Nav className="d-flex flex-grow-0 align-items-center">
+                                <div className="d-flex flex-row justify-content-center mt-3 mb-3 p-2 mb-0 bg-white-25 rounded-3">
+                                    {routes.map((route, index) => {
+                                        if (route.private && !auth.isAuthenticated()) return null;
+                                        else {
+                                            return (
+                                                <NavLink
+                                                    to={route.to}
+                                                    className="nav-link px-2 link-light">
+                                                    {route.text}</NavLink>
+                                            )
+                                        }
+                                    }
+                                    )}
+                                </div>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+                {loading ? <Spinner
+                    animation="border"
+                    variant="primary"
+                    role="status" className="position-fixed bottom-0 end-50 z-3">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner> : ""}
             </header >
         </div>
     )
