c1cd96762e4c59418e11151f4a36ff75ee3aa17e
Raúl Cuéllar <raulito1500@gmail.com>
2025-02-05 12:03:59 -0500

    Removing useless Utilities context wich was migrated to separated files no-context

--- src/App/App.js
+++ src/App/App.js
@@ -1,47 +1,44 @@
-import React from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import { AppProvider } from './Context/app';
-import { AuthProvider } from './Context/auth';
-import { Container } from 'react-bootstrap';
-import { AppHeader } from './AppHeader';
-import { AppNavBar } from './AppNavbar';
-import { Overview } from '../Pages/Overview';
-import { MarketListView } from '../Pages/MarketList/View';
-import { BillList } from '../Pages/Bill/List';
-import { BillCreate } from '../Pages/Bill/Create';
-import { NotFound } from '../Pages/NotFound';
-import { Logout } from '../Pages/Logout';
-import { BillEdit } from '../Pages/Bill/Edit';
-import { UtilitiesProvider } from './Context/utilities';
-import { ProductList } from '../Pages/Product/List';
-import { Notifications } from './Notifications';
+import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { AppProvider } from "./Context/app";
+import { AuthProvider } from "./Context/auth";
+import { Container } from "react-bootstrap";
+import { AppHeader } from "./AppHeader";
+import { AppNavBar } from "./AppNavbar";
+import { Overview } from "../Pages/Overview";
+import MarketListView from "../Pages/MarketList/View";
+import { BillList } from "../Pages/Bill/List";
+import { BillCreate } from "../Pages/Bill/Create";
+import { NotFound } from "../Pages/NotFound";
+import { Logout } from "../Pages/Logout";
+import { BillEdit } from "../Pages/Bill/Edit";
+import { ProductList } from "../Pages/Product/List";
+import { Notifications } from "./Notifications";
 
 function App() {
-  return (
-    <HashRouter>
-      <AuthProvider>
-        <AppProvider>
-          <AppHeader />
-          <AppNavBar />
-          <Notifications />
-          <UtilitiesProvider>
-            <Container className='pb-5 mb-5'>
-              <Routes>
-                <Route path='/' element={<Overview />} />
-                <Route path='/market-list/:id' element={<MarketListView />} />
-                <Route path='/bills' element={<BillList />} />
-                <Route path='/bills/create' element={<BillCreate />} />
-                <Route path='/bills/edit/:id' element={<BillEdit />} />
-                <Route path='/products' element={<ProductList />} />
-                <Route path='/logout' element={<Logout />} />
-                <Route path='*' element={<NotFound />} />
-              </Routes>
-            </Container>
-          </UtilitiesProvider>
-        </AppProvider>
-      </AuthProvider>
-    </HashRouter>
-  );
+    return (
+        <HashRouter>
+            <AuthProvider>
+                <AppProvider>
+                    <AppHeader />
+                    <AppNavBar />
+                    <Notifications />
+                    <Container className="pb-5 mb-5">
+                        <Routes>
+                            <Route path="/" element={<Overview />} />
+                            <Route path="/market-list/:id" element={<MarketListView />} />
+                            <Route path="/bills" element={<BillList />} />
+                            <Route path="/bills/create" element={<BillCreate />} />
+                            <Route path="/bills/edit/:id" element={<BillEdit />} />
+                            <Route path="/products" element={<ProductList />} />
+                            <Route path="/logout" element={<Logout />} />
+                            <Route path="*" element={<NotFound />} />
+                        </Routes>
+                    </Container>
+                </AppProvider>
+            </AuthProvider>
+        </HashRouter>
+    );
 }
 
 export default App;
